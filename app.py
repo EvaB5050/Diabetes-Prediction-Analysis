909bd9ae5eb988e342c1f38a8a3f0e7c235cacc8
EvaB5050 <135685900+EvaB5050@users.noreply.github.com>
2023-11-26 13:20:41 +1030

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ my_bar = st.progress(0, text=progress_text)
 for percent_complete in range(100):
     time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
-time.sleep(1)
-my_bar.empty()
+    time.sleep(1)
+    my_bar.empty()
 
 # progress_bar = st.progress(0)
 # for perc_completed in range(100):
